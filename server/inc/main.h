af8452d8251bfb8b318ec644af21aa12c80f3929
Valerian Vermeulen <valerian.vermeulen@epitech.eu>
2017-07-14 14:32:55 +0200

    petit set up débile pour init la branche comme il faut

---
+++ server/inc/main.h
@@ -0,0 +1,14 @@
+/*
+** main.h for jetpack2Tek3_2016 in /home/vermeu_v/Documents/Synthesis_Pool/jetpack2Tek3_2016/inc/main.h
+**
+** Made by Valerian VERMEULEN
+** Login   <valerian.vermeulen@epitech.eu>
+**
+** Started on  Fri Jul 14 08:56:33 2017 Valerian VERMEULEN
+** Last update Fri Jul 14 08:56:33 2017 Valerian VERMEULEN
+*/
+
+#ifndef JETPACK2TEK3_2016_MAIN_H
+#define JETPACK2TEK3_2016_MAIN_H
+
+#endif //JETPACK2TEK3_2016_MAIN_H
