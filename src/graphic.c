bdfc9e2703f3b8b67e3d45106016710d0e87f6d1
Loïc P <loic.pirez@epitech.eu>
2017-07-17 19:20:15 +0200

    Begin of graphical interface

--- src/graphic.c
+++ src/graphic.c
@@ -11,22 +11,40 @@
 #include <SDL2/SDL.h>
 #include <error.h>
 #include <args.h>
+#include <zconf.h>
+#include <network.h>
 #include "resolution.h"
 
 void graphic(t_thread_data *thread_data) {
-    (void)thread_data;
+    (void) thread_data;
     t_resolution *res = get_current_resolution();
     SDL_Window *pWindow = NULL;
+    SDL_Event events;
+    while (thread_data->server_data->is_ready != true);
+    int block_size = (int) (res->x / thread_data->server_data->mapX);
 
     pWindow = SDL_CreateWindow("jetpack2Tek3", SDL_WINDOWPOS_CENTERED,
                                SDL_WINDOWPOS_UNDEFINED,
-                               res->x,
-                               res->y,
+                               res->x / 2,
+                               res->y / 2,
                                SDL_WINDOW_SHOWN);
     if (pWindow) {
-        SDL_DestroyWindow(pWindow);
-    }
-    else
+        while (thread_data->server_data->is_finish != true) {
+            SDL_PollEvent(&events);
+            if (events.type == SDL_KEYDOWN) {
+                ask_server(thread_data->server_data->sock, "FIRE 1\n");
+            }
+            else {
+                ask_server(thread_data->server_data->sock, "FIRE 0\n");
+            }
+        }
+        if (thread_data->server_data->winner == -1)
+            printf("Nobody wins\n");
+        else if (thread_data->server_data->winner == thread_data->server_data->id)
+            printf("You win!\n");
+        else
+            printf("You loose!\n");
+    } else
         print_error_and_exit(ERROR_SDL, 84);
     free(res);
 }
