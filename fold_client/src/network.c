be7e4e5d1253e446ad82cbf1138bcb133a26ba32
Valerian Vermeulen <valerian.vermeulen@epitech.eu>
2017-07-19 17:53:53 +0200

    update network.c

--- fold_client/src/network.c
+++ fold_client/src/network.c
@@ -5,7 +5,7 @@
 ** Login   <loic.pirez@epitech.eu>
 **
 ** Started on  Thu Jul 13 15:28:54 2017 Loïc Pirez
-** Last update Tue Jul 18 21:10:16 2017 Loïc Pirez
+** Last update Wed Jul 19 17:31:40 2017 Loïc Pirez
 */
 
 #include <sys/socket.h>
@@ -27,49 +27,25 @@ void network(t_thread_data *thread_data)
   ask_server(thread_data, "MAP\n");
   ask_server(thread_data, "READY\n");
   if ((reply = malloc(sizeof(char) + BUFFER_SIZE)) == NULL)
-    {
-      print_error_and_exit(ERROR_MALLOC, 84);
-    }
+    print_error_and_exit(ERROR_MALLOC, 84);
   while (thread_data->server_data->is_finish != true)
     {
-      if (thread_data->server_data->is_finish == true)
-        {
-	  break;
-        }
       if ((status = recv(thread_data->server_data->sock, reply, BUFFER_SIZE, 0)) == -1)
-        {
+	{
 	  if (thread_data->server_data->is_finish == 1)
-            {
-	      display_winner_message(thread_data);
-            }
+	    display_winner_message(thread_data);
 	  print_error_and_exit(ERROR_RECV, 84);
-        }
+	}
       else if (status != 0)
-        {
+	{
 	  output = split(reply, " \n");
-
 	  check_answer(output, thread_data);
-
 	  if ((reply = malloc(sizeof(char) + BUFFER_SIZE)) == NULL)
-            {
-	      print_error_and_exit(ERROR_MALLOC, 84);
-            }
-        }
+	    print_error_and_exit(ERROR_MALLOC, 84);
+	}
       else if (strlen(reply) >= BUFFER_SIZE)
-        {
-	  print_error_and_exit(ERROR_BIGPACKET, 84);
-        }
+	print_error_and_exit(ERROR_BIGPACKET, 84);
       else
-        {
-	  break;
-        }
-    }
-  if (reply)
-    {
-      free(reply);
-    }
-  if (output)
-    {
-      free(output);
+	break;
     }
 }
